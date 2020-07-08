61647f77d0a0e1f566086193b49472cfc5cda2f0
MikeFranco <mfranco_98@yahoo.com>
2020-07-07 20:00:43 -0500

    refactor send emails

--- functions/index.js
+++ functions/index.js
@@ -32,106 +32,74 @@ exports.emailMessage = functions.https.onRequest((req, res) => {
     afterClub,
     channel
   } = req.body;
+  
+  const mailOptions = {
+    from: 'hola@hackademy.com', //useless cause' use transporter.auth.user
+    to: 'rodrigo.medina.neri@gmail.com',
+    subject: 'Nuevo Registro para el club de programación',
+    html: `
+      <p style="font-size: 16px;">Hola, aquí está la info:</p>
+      <br />
+      <ul>
+        <li>Nombre: ${name}</li>
+        <li>Email: ${email}</li>
+        <li>Celular: ${cellphone}</li>
+        <li>Ciudad y estado: ${state}</li>
+        <li>Donde estudia y semestre: ${education}</li>
+        <li>Usuario de gitHub: ${userName}</li>
+        <li>Lenguaje a aplicar: ${language}</li>
+        <li>¿Aplicará después del club?: ${afterClub}</li>
+        <li>¿Cómo se enteró?: ${channel}</li>
+      </ul>
+    `
+  };
 
-  cors(req, res, () => {
-    transporter.verify((error, success) => {
-      if (error) res.send(`Verify Error: ${error.toString()}`);
-      else {
-        console.log(
-          '%c⧭',
-          'color: #006dcc',
-          'se pasa el transporter verify emailSend'
-        );
-        const mailOptions = {
-          from: 'hola@hackademy.com', //useless cause' use transporter.auth.user
-          to: 'rodrigo.medina.neri@gmail.com',
-          subject: 'Nuevo Registro para el club de programación',
-          html: `
-            <p style="font-size: 16px;">Hola, aquí está la info:</p>
-            <br />
-            <ul>
-              <li>Nombre: ${name}</li>
-              <li>Email: ${email}</li>
-              <li>Celular: ${cellphone}</li>
-              <li>Ciudad y estado: ${state}</li>
-              <li>Donde estudia y semestre: ${education}</li>
-              <li>Usuario de gitHub: ${userName}</li>
-              <li>Lenguaje a aplicar: ${language}</li>
-              <li>¿Aplicará después del club?: ${afterClub}</li>
-              <li>¿Cómo se enteró?: ${channel}</li>
-            </ul>
-          `
-        };
-
-        return transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            return res.send(error.toString());
-          }
-          return res.send(info);
-        });
-      }
-    });
-  });
+  sendEmailFunction(req, res, mailOptions)
 });
 
 exports.newsletterFunction = functions.https.onRequest((req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
   const { email } = req.body;
 
-  cors(req, res, () => {
-    transporter.verify((error, success) => {
-      if (error) res.send(`Verify Error: ${error.toString()}`);
-      else {
-        console.log(
-          '%c⧭',
-          'color: #006dcc',
-          'se pasa el transporter verify newsletter'
-        );
-        const mailOptions = {
-          from: 'hola@hackademy.com', //useless cause' use transporter.auth.user
-          to: 'mfranco_98@yahoo.com',
-          subject: 'Nuevo Registro para el newsletter',
-          html: ` 
-            <p style="font-size: 20px;">Hola, este es el correo:</p>
-            <br />
-            <p style="font-size: 16px;">Email: ${email}</p>
-          `
-        };
+  const mailOptions = {
+    from: 'hola@hackademy.com', //useless cause' use transporter.auth.user
+    to: 'mfranco_98@yahoo.com',
+    subject: 'Nuevo Registro para el newsletter',
+    html: ` 
+      <p style="font-size: 20px;">Hola, este es el correo:</p>
+      <br />
+      <p style="font-size: 16px;">Email: ${email}</p>
+    `
+  };
 
-        return transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            return res.send(error.toString());
-          }
-          return res.send(info);
-        });
-      }
-    });
-  });
+  sendEmailFunction(req, res, mailOptions);
 });
 
 exports.enterpriseContact = functions.https.onRequest((req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
   const { name, email, message } = req.body;
+  const mailOptions = {
+    from: 'hola@hackademy.com', //useless cause' use transporter.auth.user
+    to: 'mfranco_98@yahoo.com',
+    subject: 'Nuevo contacto para empresa',
+    html: `
+    <p style="font-size: 16px;">Hola, aquí está la info:</p>
+    <br />
+    <ul>
+      <li>Nombre: ${name}</li>
+      <li>Email: ${email}</li>
+      <li>Mensaje: ${message}</li>
+    </ul>
+  `
+  };
+  sendEmailFunction(req, res, mailOptions);
+});
 
+const sendEmailFunction = (req, res, mailOptions) => {
   cors(req, res, () => {
     transporter.verify((error, success) => {
       if (error) res.send(`Verify Error: ${error.toString()}`);
       else {
-        const mailOptions = {
-          from: 'hola@hackademy.com', //useless cause' use transporter.auth.user
-          to: 'mfranco_98@yahoo.com',
-          subject: 'Nuevo contacto para empresa',
-          html: `
-          <p style="font-size: 16px;">Hola, aquí está la info:</p>
-          <br />
-          <ul>
-            <li>Nombre: ${name}</li>
-            <li>Email: ${email}</li>
-            <li>Mensaje: ${message}</li>
-          </ul>
-        `
-        };
-
         return transporter.sendMail(mailOptions, (error, info) => {
           if (error) {
             return res.send(error.toString());
@@ -141,4 +109,4 @@ exports.enterpriseContact = functions.https.onRequest((req, res) => {
       }
     });
   });
-});
+}
